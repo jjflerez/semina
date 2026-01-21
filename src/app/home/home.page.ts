cd7760e7c2c3419db063e6bf4c7623f4e5fdaf6b
jjflerez <jose.cabarcasc@pca.edu.co>
2026-01-20 20:32:32 -0500

    ejercicio de storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+    selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
+  colorclaro = 'var(--color-claro)';
+  coloroscuro = 'var(--color-oscuro)';
+  coloractual = this.coloroscuro;
+
+  temaOscuro = false;
+  cambiarTema() {
+    this.temaOscuro = !this.temaOscuro;
+    document.documentElement.style.setProperty(
+      '--slide-bg',
+      this.temaOscuro
+        ? 'var(--slide-color-oscuro)'
+        : 'var(--slide-color-claro)'
+    );
+    document.documentElement.style.setProperty(
+      '--slide-text',
+      this.temaOscuro
+        ? 'var(--slide-text-oscuro)'
+        : 'var(--slide-text-claro)'
+    );
+  }
+  
+  genres = [
+    {
+      Title: 'Musica Clasica',
+      Image: 'https://images.unsplash.com/photo-1526142684086-7ebd69df27a5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bXVzaWNhJTIwY2xhc2ljYXxlbnwwfHwwfHx8MA%3D%3D',
+      Description: 'La música clásica es un género musical que abarca un amplio período de tiempo y se caracteriza por su complejidad estructural y riqueza armónica. Compositores como Mozart, Beethoven y Bach son figuras destacadas de este género',
+    },
+    {
+      Title: 'Salsa',
+      Image: 'https://static.vecteezy.com/system/resources/thumbnails/023/139/989/small/abstract-illustration-featuring-trumpet-and-colored-ornaments-ai-generated-photo.jpg',
+      Description: 'La salsa musical tiene sus raíces en la fusión de ritmos afrocubanos como el Son Cubano, Mambo y Cha-cha-chá, enriquecidos con Jazz y otros estilos, naciendo en Cuba pero consolidándose y popularizándose globalmente en Nueva York a finales de los años 60 y 70, gracias a la inmigración cubana y puertorriqueña, y a músicos que la bautizaron con este nombre comercial para englobar estos sonidos caribeños. ',
+    },
+    {
+      Title: 'Vallenato',
+      Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOilHWC40h1AJHs-n7ekkba9d8MyRMnAqGrg&s',
+      Description: 'El vallenato nació en la región Caribe de Colombia, especialmente en Valledupar, como una fusión de culturas: cantos de vaqueros, ritmos africanos (tambores) e indígenas (guacharaca), y la llegada del acordeón europeo a mediados del siglo XIX. Originalmente, los juglares o "vallenateros" cantaban historias de la vida cotidiana, el amor y las leyendas locales.',
+    },
+  ];
   constructor() {}
+
+  Cambiarcolor() {
+    this.coloractual = this.coloractual === this.coloroscuro ? this.colorclaro : this.coloroscuro;
+  }
+
+   
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+
+import{Storage} from '@ionic/storage-angular';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
