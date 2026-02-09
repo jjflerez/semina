04fd23bd6c56c0a4acbf5822835fbbc9b170ac5e
jjflerez <jose.cabarcasc@pca.edu.co>
2026-02-09 05:14:44 -0500

    menu

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {  IonicModule } from '@ionic/angular';
-
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
@@ -12,7 +12,11 @@ import {  IonicModule } from '@ionic/angular';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
+  
+  async cerrarSesion() {
+    await this.storage.clear();
+  }
 
   ngOnInit() {
   }
